74b2d8892932f3c69d8946d25d9bb9e7f433aa53
Craig Monaghan <158125396+craigmonaghan@users.noreply.github.com>
2025-04-08 04:26:17 +0100

    Update main.js

--- RPS/main.js
+++ RPS/main.js
@@ -1,19 +1,4 @@
-// Codeacademy project function 
-// function getUserChoice(userInput) {
-//     userInput;
-//     userInput = userInput.toLowerCase();
-  //   if (
-  //     userInput === "rock" ||
-  //     userInput === "paper" ||
-  //     userInput === "scissors"
-  //   ) {
-  //     return userInput;
-  //   } else {
-  //     console.log("Error!");
-  //   }
-  // }
-  
-  function getComputerChoice() {
+function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber === 0) return "rock";
     else if (randomNumber === 1) return "paper";
